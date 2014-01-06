592f1e14863171ae56250f310fcdcca50b058f16
Ari Ruotsalainen <ari.ruotsalainen@solita.fi>
2014-01-06 19:47:56 +0000

    Initial draft. Just to put something

---
+++ kahvipoliisi/capture.py
@@ -0,0 +1,70 @@
+import cv2, time
+from threading import Thread
+from SocketServer import ThreadingMixIn
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from os import curdir, sep
+
+cam = None
+
+camera_index = 0
+width = 320
+height = 240
+
+def initialize_camera():
+	global cam
+	global width
+	global height
+        cam = cv2.VideoCapture()
+        cam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+        cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
+        cam.open(0)
+        height = cam.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+        width = cam.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+
+def release_camera():
+	cam.release()
+
+def cap():
+	#time.sleep(3)
+	f,image = cam.read()
+	if not f:
+		exit(1)
+	#f,image = cam.read()
+	cv2.imwrite("capture.jpg", image)
+	print "Captured", width, height
+
+def capture_forever():
+	while True:
+		cap()
+		time.sleep(1)
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+	try:
+		if self.path.endswith("capture.jpg"):
+			f = open(curdir + sep + self.path, 'rb')
+		        self.send_response(200)
+        		self.send_header("Content-type", "image/jpeg")
+		        self.end_headers()
+	       	 	self.wfile.write(f.read())
+			f.close()
+			return
+	except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+
+def serve_on_port(host, port):
+	print "Serving in", host, port
+	server = ThreadingHTTPServer((host, port), Handler)
+	server.serve_forever()
+	
+if __name__ == "__main__":
+	t = Thread(target=serve_on_port, args=["0.0.0.0", 1111])
+	t.daemon = True
+	t.start()
+	initialize_camera()
+	capture_forever()
+	release_camera()
+	
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kahvipoliisi'
+	version='0.1',
+	description='Try to build better coffee culture by nagging',
+	url='https://github.com/ruoat/kahvipoliisi',
+	autho'Ari Ruotsalainen',
+	author_email='ari.ruotsalainen@solita.fi',
+	license='MIT',
+	packages=['kahvipoliisi'],
+	zip_safe=False)
